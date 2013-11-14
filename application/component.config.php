7673fb50d67ac356977160bbdf57cb0c5742a712
Vladislav Bakin <vladislav.bakin@umisoft.ru>
2013-11-14 11:26:44 +0400

    fix routing language mask

--- application/component.config.php
+++ application/component.config.php
@@ -2,10 +2,12 @@
 namespace application;
 
 use umi\hmvc\component\IComponent;
+use umi\route\IRouteFactory;
 
 return [
     // класс MVC-компонента
     'componentClass'               => __NAMESPACE__ . '\Application',
+
     // зарегистрированные контроллеры
     IComponent::OPTION_CONTROLLERS => [
         IComponent::ERROR_CONTROLLER   => __NAMESPACE__ . '\controller\ErrorController',
@@ -13,10 +15,12 @@ return [
 
         'index'                        => __NAMESPACE__ . '\controller\IndexController',
     ],
+
     // зарегистрированные модели
     IComponent::OPTION_MODELS      => [
         'user' => 'app\components\auth\model\UserModel'
     ],
+
     // настройки шаблонизатора
     IComponent::OPTION_VIEW        => [
         // тип шаблонизатора
@@ -25,14 +29,20 @@ return [
         // путь до шаблонов компонента
         'directory' => __DIR__ . '/view',
     ],
+
     // маршруты текущего компонента
     IComponent::OPTION_ROUTES      => [
         // базовый маршрут внутри компонента
         'home' => [
             // тип маршрута - на основе simple-выражений
-            'type'     => 'simple',
+            'type'     => IRouteFactory::ROUTE_EXTENDED,
             // путь(маска) маршрута
             'route'    => '/{lang}',
+
+            'rules' => [
+                'lang' => '[a-z]{2}-[A-Z]{2}'
+            ],
+
             'defaults' => [
                 'lang'       => 'ru-RU',
                 'controller' => 'index'
